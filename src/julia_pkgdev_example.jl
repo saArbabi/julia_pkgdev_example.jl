fc9fad3b1d8050e50f34d94679e519a5b5c3ff33
Salar <salar.arbabi.i@gmail.com>
2020-05-02 15:44:52 +0100

    added the my_f function
    my_f is linear function for demonstraiton

--- src/julia_pkgdev_example.jl
+++ src/julia_pkgdev_example.jl
@@ -1,5 +1,10 @@
 module julia_pkgdev_example
 
 greet() = print("Hello World!")
+include("add_fun.jl")
+export my_f
 
 end # module
+
+# add include("isbfviub.jl") type scripts
+# if you want these accessable when you  use the module, export them
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using julia_pkgdev_example
 using Test
 
+
 @testset "julia_pkgdev_example.jl" begin
     # Write your own tests here.
+    @test my_f(2,1) == 5
+    @test my_f(2,5) == 9
+
 end
